9af1cb9e33bd6a53fa433e16daf3ebef0afc83c8
farahhfarah <farahf@roehampton.ac.uk>
2024-08-20 12:19:16 +0100

    Initial Commit

---
+++ safewalk/safewalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  safewalkApp.swift
+//  safewalk
+//
+//  Created by F Farah on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct safewalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
